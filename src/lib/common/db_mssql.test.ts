a31a59e73401d8d6e1be8d4f7870f981a8cf6fd1
Steve Gallagher <steveg0493@gmail.com>
2025-07-29 22:34:50 -0700

    Create tests for db_mssql

---
+++ src/lib/common/db_mssql.test.ts
@@ -0,0 +1,71 @@
+import sql from "mssql";
+import { DBInstance, SQL_TYPES } from "$lib/common/db_mssql";
+
+import { describe, it, expect } from "vitest";
+
+describe("query", async () => {
+  let department_id: number;
+  let department_name: string;
+
+  it("should insert a new department", async () => {
+    const dbContext = await (await DBInstance.getInstance()).getContext();
+
+    const sqlText = `insert into data.department (department_name) output inserted.* values (@department_name)`;
+    const result = await dbContext
+      .request()
+      .input("department_name", SQL_TYPES.VarChar(255), "Test")
+      .query(sqlText);
+    console.log(result);
+    department_id = result.recordset[0].department_id;
+    department_name = result.recordset[0].department_name;
+    expect(result.recordset[0].department_name).toBe("Test");
+  });
+
+  it("should return a result with an id of the inserted department", async () => {
+    const dbContext = await (await DBInstance.getInstance()).getContext();
+
+    const sqlText = `select department_id, department_name from data.department where department_id = @department_id`;
+    const result = await dbContext
+      .request()
+      .input("department_id", SQL_TYPES.Int, department_id)
+      .query(sqlText);
+    console.log(result);
+    expect(result.recordset[0].department_id).toBe(department_id);
+  });
+
+  it("should return a result with a department name of the inserted department", async () => {
+    const dbContext = await (await DBInstance.getInstance()).getContext();
+
+    const sqlText = `select department_id, department_name from data.department where department_name = @department_name`;
+    const result = await dbContext
+      .request()
+      .input("department_name", SQL_TYPES.VarChar(255), department_name)
+      .query(sqlText);
+    console.log(result);
+    expect(result.recordset[0].department_name).toBe(department_name);
+  });
+
+  it("should update a department", async () => {
+    const dbContext = await (await DBInstance.getInstance()).getContext();
+
+    const sqlText = `update data.department set department_name = @department_name output inserted.* where department_id = @department_id`;
+    const result = await dbContext
+      .request()
+      .input("department_name", SQL_TYPES.VarChar(255), "Test123")
+      .input("department_id", SQL_TYPES.Int, department_id)
+      .query(sqlText);
+    console.log(result);
+    expect(result.recordset[0].department_name).toBe("Test123");
+  });
+
+  it("should delete a department", async () => {
+    const dbContext = await (await DBInstance.getInstance()).getContext();
+
+    const sqlText = `delete from data.department where department_id = @department_id`;
+    const result = await dbContext
+      .request()
+      .input("department_id", SQL_TYPES.Int, department_id)
+      .query(sqlText);
+    expect(result.rowsAffected[0]).toBe(1);
+  });
+});
