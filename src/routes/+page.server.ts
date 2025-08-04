af7925863ee08d045b9889a193b2e468e460f0df
Steve Gallagher <steveg0493@gmail.com>
2025-08-03 17:12:32 -0700

    Add Department typing to home page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,9 @@
 import { db } from "$lib/db.server";
+import type { Department } from "$lib/types/Department";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
-  const departments = await db.department.findMany();
+  const departments: Department[] = await db.department.findMany();
 
   return {
     departments,
