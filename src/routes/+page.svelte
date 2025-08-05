7247becc723b387e2f928c880a3a20b5c87de3cd
Steve Gallagher <steveg0493@gmail.com>
2025-08-05 11:42:05 -0700

    Minor formatting and comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,15 +15,20 @@
         
     })
 </script>
+
 {#snippet button(text: string, onclick: any)}
     <button type="button" {onclick} class="h-9 px-2 rounded-md bg-slate-500 text-white font-bold hover:brightness-90">{text}</button>
 {/snippet}
+
 <h1 class="text-2xl">Departments</h1>
+
+<!-- Search and add buttons -->
 <div class="mb-4">
     <input type="text" bind:value={searchTerm} placeholder="Search..." class="border border-zinc-400 rounded-md h-9 p-2"/>
     {@render button('Search', ()=>{})}
     {@render button('Add', ()=>{goto('/department/0')})}
 </div>
+
 <!-- List departments -->
 {#each filteredResults as department}
     <div class="flex odd:bg-zinc-200 items-center gap-3 p-2">
