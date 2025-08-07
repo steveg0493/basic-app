815350744de455cfd14e65bfadf94ffd77e969f4
Steve Gallagher <steveg0493@gmail.com>
2025-08-06 19:48:24 -0700

    Change the drizzle setup to use a pool rather than the newer syntax with just the url

--- src/lib/common/db.ts
+++ src/lib/common/db.ts
@@ -1,3 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import { DATABASE_URL } from "$env/static/private";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  idleTimeoutMillis: 15000,
+  connectionTimeoutMillis: 5000,
+  max: 50,
+});
+
+export const db = drizzle(pool);
