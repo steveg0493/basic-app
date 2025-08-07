f034449519f8fd37199f94fcdd578f11d0fa1d1f
Steve Gallagher <steveg0493@gmail.com>
2025-08-06 20:21:32 -0700

    Add a sort to the list of departments

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,13 @@
 import { db } from "$lib/common/db";
 import type { PageServerLoad } from "./$types";
 import { DepartmentTable } from "$lib/db/schema";
+import { asc } from "drizzle-orm";
 
 export const load: PageServerLoad = async () => {
-  const departments = await db.select().from(DepartmentTable);
+  const departments = await db
+    .select()
+    .from(DepartmentTable)
+    .orderBy(asc(DepartmentTable.department_name));
 
   return {
     departments,
