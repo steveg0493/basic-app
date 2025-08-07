2cf484fafd5430b710486b064d73d5945f769e53
Steve Gallagher <steveg0493@gmail.com>
2025-08-06 19:49:21 -0700

    Modify the /page.server.ts to use drizzle

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,11 @@
-import { Department } from "$lib/server/department";
+import { db } from "$lib/common/db";
 import type { PageServerLoad } from "./$types";
+import { DepartmentTable } from "$lib/db/schema";
 
 export const load: PageServerLoad = async () => {
-    const departments = await Department().getAll()
+  const departments = await db.select().from(DepartmentTable);
 
-    return {
-        departments
-    }
-}
+  return {
+    departments,
+  };
+};
